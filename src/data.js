baeb0e8e3b513f7bbd81547b43f0e576b790106a
Melford-D <melfordbirakor@gmail.com>
2021-04-12 11:39:03 +0100

    Updated the image links

--- src/data.js
+++ src/data.js
@@ -2,32 +2,32 @@ export const imageData = [
     {
         id: 1,
         name: 'makeup-pictures',
-        url: 'https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/141513337_437459484123463_7697780776564686617_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=CgMFhFuEm58AX-hDg5R&tp=1&oh=187eba252d9af295bc879178aefd6773&oe=6035F034'
+        url: 'https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/128645743_1087845554970580_2919344977081286439_n.jpg?tp=1&_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=G5pgC3zsW5MAX-iWgQQ&edm=AP_V10EAAAAA&ccb=7-4&oh=9f7dc318b3e2d50abbce17c5563bc43b&oe=6098AA5E&_nc_sid=4f375e'
     },
     {
         id: 2,
         name: 'makeup-pictures',
-        url: 'https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/128645743_1087845554970580_2919344977081286439_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=SD16Pw06W94AX8k5gUl&tp=1&oh=30554cf9d16a9eb886bacce054168a01&oe=60375EDE'
+        url: 'https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/142656973_463833874639591_3978294671712381547_n.jpg?tp=1&_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=cK85X6gClesAX9srHGD&edm=AP_V10EAAAAA&ccb=7-4&oh=2edff220575dff2d86039a19e8808dbf&oe=609968B2&_nc_sid=4f375e'
     },
     {
         id: 3,
         name: 'makeup-pictures',
-        url: 'https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/128127700_1749665431861059_321683194129459160_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=N8yH-k2p-XwAX-RYtjh&tp=1&oh=ca812136b89071f8c0fad9c174856faf&oe=6037E5FE'
+        url: 'https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/141513337_437459484123463_7697780776564686617_n.jpg?tp=1&_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=3ibIOAp_u2IAX_X-YxL&edm=AP_V10EAAAAA&ccb=7-4&oh=9d963e89166afe481169b07273852dc4&oe=6098A538&_nc_sid=4f375e'
     },
     {
         id: 4,
         name: 'makeup-pictures',
-        url: 'https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/124846938_712045376411511_2642231047224398388_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=E5WXObGBlFsAX_c260U&tp=1&oh=804492dbba27d93ff97306d63469353e&oe=6037C213'
+        url: 'https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/167307610_367762821047041_5537921389755520546_n.jpg?tp=1&_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=kqnwGBhgbrMAX-O_cyF&edm=AP_V10EAAAAA&ccb=7-4&oh=86e162363ce69781b4b73e398aa0dbd9&oe=60985AC1&_nc_sid=4f375e'
     },
     {
         id: 5,
         name: 'makeup-pictures',
-        url: 'https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/121479934_198988674967984_2705903690356444881_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=zYkTi3mgfUcAX9YbpFx&tp=1&oh=95444385a7a7c8bdc6fea4bea9831981&oe=60373252'
+        url: 'https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/128127700_1749665431861059_321683194129459160_n.jpg?tp=1&_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=Gr5UXTdMAUAAX8Fczym&edm=AP_V10EAAAAA&ccb=7-4&oh=b3d35d7d5101986f8c25a7b8970ec0dc&oe=609AC67E&_nc_sid=4f375e'
     },
     {
         id: 6,
         name: 'makeup-pictures',
-        url: 'https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/119866162_2804896359794912_7889201816008610581_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=YpSnIiBt-rwAX-PVRXa&tp=1&oh=a3a3cd11a5c16a797c45bd67ee37882d&oe=60355208'
+        url: 'https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/118979588_241508840508994_4464979171431392865_n.jpg?tp=1&_nc_ht=instagram.flos1-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=QnfEuVYRdYQAX9MJG4u&edm=AP_V10EAAAAA&ccb=7-4&oh=98c89be14bcb299346b36e30a7e7d5a5&oe=6097F6A6&_nc_sid=4f375e'
     },
 ]
 
@@ -35,7 +35,7 @@ export const ceoImage = [
     {
         id: 1,
         name: 'Mrs Dumsira Menegbo',
-        url: 'https://scontent.flos1-1.fna.fbcdn.net/v/t1.0-9/126882470_3271663359629440_6691126653650370989_o.jpg?_nc_cat=111&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeGjim5wrGXc2_2CHCcc8gXj9rGp8PMeOdX2sanw8x451VzSNbyumIvpJXfPoDeMnpBz4lRU2Zm6piahU5r9bfGL&_nc_ohc=pOpPnBAj3XIAX-vkfRH&_nc_ht=scontent.flos1-1.fna&oh=ef9e2df0ca820ed6cef8b56df4ca970d&oe=60326C13',
+        url: 'https://scontent.flos1-1.fna.fbcdn.net/v/t1.6435-9/126882470_3271663359629440_6691126653650370989_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeGjim5wrGXc2_2CHCcc8gXj9rGp8PMeOdX2sanw8x451VzSNbyumIvpJXfPoDeMnpBz4lRU2Zm6piahU5r9bfGL&_nc_ohc=p469til2-n0AX_JcbrW&_nc_ht=scontent.flos1-1.fna&oh=27d585b25843eccb73eae565eac998c6&oe=609992C2',
         details: "Hi!, I am Dumsira. It's my pleasure meeting with you to showcase my designs to you. My company is known for delivering high quality makeup services as well as outdoor services like weddings, anniversary, etc. We look forward hearing from you. Thank you." 
     }
 ]
